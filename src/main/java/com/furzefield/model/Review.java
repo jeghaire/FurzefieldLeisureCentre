d26e93240d4395108f3c484d49f4cf84e9c58d78
Eghaire, Praise Jomavi <jomaviprz@gmail.com>
2026-03-11 02:43:23 +0000

    Implemented getRatingLabel in Review

--- src/main/java/com/furzefield/model/Review.java
+++ src/main/java/com/furzefield/model/Review.java
@@ -22,9 +22,21 @@ public class Review {
     public int getRating() { return rating; }
     public String getComment() { return comment; }
 
+    public String getRatingLabel() {
+        return switch (rating) {
+            case 1 -> "Very Dissatisfied";
+            case 2 -> "Dissatisfied";
+            case 3 -> "Ok";
+            case 4 -> "Satisfied";
+            case 5 -> "Very Satisfied";
+            default -> "Unknown";
+        };
+    }
+
     @Override
     public String toString() {
         return member.getName() + " rated " + lesson.getExerciseType().getDisplayName()
-                + " " + rating + "/5 - \"" + comment + "\"";
+                + " " + rating + "/5 (" + getRatingLabel() + ")"
+                + " - \"" + comment + "\"";
     }
 }
